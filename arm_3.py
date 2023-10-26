eef85cacf5a70145a4e0a1b378c3817c216701ff
Sina-Haz <101819919+Sina-Haz@users.noreply.github.com>
2023-10-26 18:46:28 -0400

    arm 3 is buggy?

--- arm_3.py
+++ arm_3.py
@@ -1,6 +1,9 @@
 import argparse
 from planar_arm import Arm_Controller
 from math import radians
+from create_scene import create_plot
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
 
 # Interpolate points on line segment from start to goal given some small resolution as a step size
 def interpolate(start, goal, resolution):
@@ -20,6 +23,12 @@ def interpolate(start, goal, resolution):
         points.append(goal)
     return points
 
+def update(frame, arm, points):
+    if frame < len(points):
+        arm.theta1, arm.theta2 = points[frame]
+        arm.re_orient()
+        arm.draw_arm()
+
 # Usage: python3 arm_3.py --start 0 0 --goal 2 2
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='arm_3 will interpolate points from start to goal configurations and show the arm at these points')
@@ -27,5 +36,24 @@ if __name__ == '__main__':
     parser.add_argument('--goal', nargs=2, type=float, required=True, help='Goal configuration, given as two floats in radians')
     args = parser.parse_args()
 
-    print(interpolate(args.start, args.goal, radians(5)))
+    # We discretize line segment in c-space according to resolution of 5 degrees
+    discretized_pts = interpolate(args.start, args.goal, radians(5))
+
+    fig, ax = plt.subplots(dpi=100)
+    planar_arm = Arm_Controller(args.start[0],args.start[1],ax)
+    
+    for pt in discretized_pts:
+        planar_arm.theta1=pt[0]
+        planar_arm.theta2=pt[1]
+        planar_arm.re_orient()
+        planar_arm.draw_arm()
+
+
+
+    # anim = FuncAnimation(fig, update, frames=len(discretized_pts), fargs=(planar_arm,discretized_pts), repeat=False, blit=False)
+    # plt.show()    
+
+
+
+    
 
